650b04e182dbb14bf500bbfb24687e01c1a1b7fd
Michael Duke <michaelcduke@gmail.com>
2016-01-06 15:43:03 -0500

    Complete p04_session_spec.

--- lib/controller_base.rb
+++ lib/controller_base.rb
@@ -1,5 +1,6 @@
 require 'active_support'
 require 'active_support/core_ext'
+require 'active_support/inflector'
 require 'erb'
 require_relative './session'
 require 'byebug'
@@ -22,6 +23,7 @@ class ControllerBase
   # Set the response status code and header
   def redirect_to(url)
     raise "Can't render twice" if already_built_response?
+    session.store_session(res)
     @res.header["location"] = url
     @res.status = 302
     @already_built_response = true
@@ -32,6 +34,7 @@ class ControllerBase
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
     raise "Can't render twice" if already_built_response?
+    session.store_session(res)
     @res['Content-Type'] = content_type
     @res.body = [content]
     @already_built_response = true
@@ -40,10 +43,15 @@ class ControllerBase
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+    controller_name = self.class.to_s.underscore
+    template_file = File.read("views/#{controller_name}/#{template_name}.html.erb")
+    erb = ERB.new(template_file).result(binding)
+    render_content(erb, 'text/html')
   end
 
   # method exposing a `Session` object
   def session
+    @session ||= Session.new(req)
   end
 
   # use this with the router to call action_name (:index, :show, :create...)
--- lib/session.rb
+++ lib/session.rb
@@ -1,19 +1,29 @@
 require 'json'
+require 'byebug'
 
 class Session
   # find the cookie for this app
   # deserialize the cookie into a hash
   def initialize(req)
+    if req.cookies["_rails_lite_app"]
+      @session = JSON.parse(req.cookies["_rails_lite_app"])
+    else
+      @session = {}
+    end
   end
 
   def [](key)
+    @session[key]
   end
 
   def []=(key, val)
+    @session[key] = val
   end
 
   # serialize the hash into json and save in a cookie
   # add to the responses cookies
   def store_session(res)
+    cookie_attr = {path: "/", value: @session.to_json}
+    res.set_cookie("_rails_lite_app", cookie_attr)
   end
 end
