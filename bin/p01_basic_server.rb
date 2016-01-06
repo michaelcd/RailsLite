f9403ac3b530139fd2adc333cc2c1e43b7244cb9
Michael Duke <michaelcduke@gmail.com>
2016-01-06 14:12:15 -0500

    Complete P01 Rack warmup.

--- bin/p01_basic_server.rb
+++ bin/p01_basic_server.rb
@@ -1,1 +1,17 @@
 require 'rack'
+require 'byebug'
+
+app = Proc.new do |env|
+  req = Rack::Request.new(env)
+  res = Rack::Response.new
+  res['Content-Type'] = 'text/html'
+  res.write(env["PATH_INFO"])
+  # byebug
+  res.finish
+
+end
+
+Rack::Server.start(
+  app: app,
+  Port: 3000
+)
