f38406e23651184420072ca2667216882df2a5df
Michael Duke <michaelcduke@gmail.com>
2016-01-06 17:38:59 -0500

    Create Flash class and initialize method.

---
+++ lib/flash.rb
@@ -0,0 +1,18 @@
+class Flash
+
+  def initialize(req)
+    if req.cookies["flash"]
+      @flash = JSON.parse(req.cookies["flash"])
+    else
+      @flash = {}
+    end
+  end
+
+  def [](key)
+
+  end
+
+  def [](key, value)
+
+  end
+end
